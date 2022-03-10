8a6ee45b44f002b44fd3723c393c73c09109626c
urther <alswlkku@ajou.ac.kr>
2022-03-11 00:56:08 +0900

    🚀  chore : install redux-thunk

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import Routers from "./Routes/Routers";
 import Navigator from "./Components/Header/Navigator";
 import Footer from "./Components/Footer/Footer";
-import { authService, dbService } from "./Services/firebase";
 
-const App = () => {
+const App = (props) => {
   // console.log(authService.currentUser); -> 로그인 여부 체크
+  // let state = useSelector((state) => state);
+  // let dispatch = useDispatch();
+
+  // useEffect(() => {
+  //   dispatch(fetchItemsData());
+  // }, [dispatch]);
+
   return (
     <>
       <Navigator />
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import App from "./App.jsx";
-import store from "./Services/reducer";
+import store from "./Services/store";
 
 ReactDOM.render(
   <React.StrictMode>
